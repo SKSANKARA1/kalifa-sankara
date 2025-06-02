2dd31d45ebb5f0f80face43ab29f5d87548ed2d7
Mehdi Nour <mehdi.nour@altergaz.local>
2025-06-02 13:26:01 +0200

    modifier les chemins des images pour lecture correct

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://mnour01.github.io/kalifa-sankara",
-  base: "/kalifa-sankara/",
+  site: "https://www.sambokalifasankara.com",
+  base: "/",
   output: "static"
 });
-
-
-
